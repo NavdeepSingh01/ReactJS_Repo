b680ea8f1bb10272032c787de5e6a91679ea0bf1
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2025-03-18 12:42:16 +0000

    counterproject

---
+++ 02conter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setCounter]=useState(0)
+
+  function addOne(){
+    setCounter(counter+1)
+  }
+  function reduceOne(){
+    setCounter(counter-1)
+  }
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addOne} >Increment</button>
+      <br />
+      <button onClick={reduceOne}>Decreament</button>
+    </>
+  )
+}
+
+export default App
